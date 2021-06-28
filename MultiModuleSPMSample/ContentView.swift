93e687bd7060c3285c97a000fccd2b9cf2704a6c
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-28 13:31:55 +0900

    Initial Commit

---
+++ MultiModuleSPMSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiModuleSPMSample
+//
+//  Created by Yusuke Hasegawa on 2021/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiModuleSPMSample/MultiModuleSPMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiModuleSPMSampleApp.swift
+//  MultiModuleSPMSample
+//
+//  Created by Yusuke Hasegawa on 2021/06/28.
+//
+
+import SwiftUI
+
+@main
+struct MultiModuleSPMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
